9d24e1f7098f1759fe9de652dce5d3bcfc7aa52f
GuzzDoritos <gustavo.bragada@hotmail.com>
2026-04-06 20:01:07 -0300

    Soluciona o problema 1 da atividade 1 de ORD

---
+++ organizacao_e_recuperacao_de_dados/campos_e_registros/programa1.py
@@ -0,0 +1,29 @@
+def main():
+    escreve_campos()
+
+def escreve_campos():
+    FILE_NAME = input('Informe o nome do arquivo:')
+    saida = open(FILE_NAME, 'w', encoding='utf-8')
+    SOBRENOME = input('Informe o seu sobrenome')
+    listaInfo = [
+        'Digite o seu nome:', 
+        'Digite a sua cidade', 
+        'Digite o seu endereço', 
+        'Digite o seu estado', 
+        'Digite o seu CEP']
+
+    while SOBRENOME:
+        infoValores = []
+        infoValores.append(SOBRENOME+"|")
+
+        for info in listaInfo:
+            valor = input(info)
+            infoValores.append(valor+'|')
+        
+        saida.writelines(infoValores)
+
+        SOBRENOME = input('Informe o seu sobrenome')
+    saida.close();
+
+if __name__ == "__main__":
+    main()
