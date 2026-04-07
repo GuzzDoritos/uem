cfc32a4a11f61ca7bf03d6eba37d0a60262fd1d8
GuzzDoritos <gustavo.bragada@hotmail.com>
2026-04-07 20:32:36 -0300

    ord: resolve problema 3 da atividade 1

---
+++ organizacao_e_recuperacao_de_dados/campos_e_registros/programa3.py
@@ -0,0 +1,38 @@
+def main():
+    escreve_registros()
+
+def escreve_registros():
+    NOME_ARQ = input('Informe o nome do arquivo a ser lido: ')
+    try:
+        SAIDA = open(NOME_ARQ, 'wb')
+    except:
+        raise FileNotFoundError(f"Arquivo {NOME_ARQ} não encontrado no diretório.")
+    
+    CAMPO = input("Digite o seu sobrenome: ")
+    while CAMPO:
+        BUFFER = ""
+        BUFFER += f"{CAMPO}|"
+        campos = [
+            'Digite o seu nome: ', 
+            'Digite a sua cidade: ', 
+            'Digite o seu endereço: ', 
+            'Digite o seu estado: ', 
+            'Digite o seu CEP: ']
+        
+        for campo in campos:
+            CAMPO = input(campo)
+            BUFFER += f"{CAMPO}|"
+        
+        BUFFER = BUFFER.encode()
+        TAM = len(BUFFER).to_bytes(2, 'little')
+
+        SAIDA.write(TAM)
+        SAIDA.write(BUFFER)
+
+        CAMPO = input("Digite o seu sobrenome: ")
+
+    SAIDA.close()
+
+
+if __name__ == "__main__":
+    main()
